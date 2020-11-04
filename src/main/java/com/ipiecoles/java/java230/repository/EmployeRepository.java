7f84755f258bd674516d58cc521f524d9920cbce
antobrines <antonin.brines@outlook.fr>
2020-11-04 09:01:07 +0100

    Exo414, test pass

--- src/main/java/com/ipiecoles/java/java230/repository/EmployeRepository.java
+++ src/main/java/com/ipiecoles/java/java230/repository/EmployeRepository.java
@@ -1,7 +1,9 @@
 package com.ipiecoles.java.java230.repository;
 
 import com.ipiecoles.java.java230.model.Employe;
+import org.joda.time.LocalDate;
 import org.springframework.data.repository.CrudRepository;
+
 
 import java.util.List;
 
@@ -11,4 +13,6 @@ public interface EmployeRepository extends CrudRepository<Employe,Long> {
     List<Employe> findByNomAndPrenom(String nom, String prenom);
 
     List<Employe> findByNomIgnoreCase(String nom);
+
+    List<Employe> findByDateEmbaucheBefore(LocalDate date);
 }
